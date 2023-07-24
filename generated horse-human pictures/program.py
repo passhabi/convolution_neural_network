63a485ae77cd2883ecff4bf4e2a37e736bfef564
Pedram <pedramas@live.com>
2023-07-24 12:48:52 +0330

    Prediction on real data with %100 model accruary only on train data

--- generated horse-human pictures/program.py
+++ generated horse-human pictures/program.py
@@ -1,10 +1,11 @@
 # download the zip files:
 # https://storage.googleapis.com/tensorflow-1-public/course2/week3/horse-or-human.zip
 # https://storage.googleapis.com/tensorflow-1-public/course2/week3/validation-horse-or-human.zip
-
+import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from zipfile import ZipFile
+import os
 
 # extracting the data
 with ZipFile('horse-or-human.zip') as zipfile:
@@ -28,31 +29,65 @@ valid_dataset = tf.keras.utils.image_dataset_from_directory(
     label_mode='binary'
 )
 
+# datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255)
+# train_data_gen = datagen.flow_from_dataframe(
+#     'train-data',
+#     target_size=(300, 300, 1),
+#     batch_size=128,
+#     class_mode='binary'
+# )
+
 
 # creating the model:
 model = tf.keras.Sequential([
     tf.keras.layers.Rescaling(scale=1/255),
-    tf.keras.layers.Conv2D(filters=16,
+    tf.keras.layers.Conv2D(filters=8,
                            kernel_size=(3, 3),
                            input_shape=(300, 300, 3),
                            activation='relu'),
     tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
 
-    tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu'),
     tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
 
     tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
+
+    tf.keras.layers.Conv2D(filters=64, kernel_size=(1, 1), activation='relu'),
     tf.keras.layers.MaxPool2D(pool_size=(2, 2)),
 
     tf.keras.layers.Flatten(),
     # tf.keras.layers.Rescaling(scale=1 / 255),
-    tf.keras.layers.Dense(units=128, activation='relu'),
-    tf.keras.layers.Dense(units=10, activation='softmax'),
+    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=1, activation='sigmoid'),
 ])
-model.build(trai)
+
+model.build((None, 300, 300, 3))
+
 model.summary()
-model.compile(loss=tf.keras.losses.sparse_categorical_crossentropy,
-              optimizer=tf.keras.optimizers.Adam(learning_rate=0.0055, ),
+model.compile(loss=tf.keras.losses.binary_crossentropy,
+              optimizer=tf.keras.optimizers.Adam(),
               metrics=['accuracy'], )
 
-model.fit(x, y, batch_size=1024,  epochs=20)  # loss: 0.0680 - accuracy: 0.9745
+model.fit(train_dataset, epochs=12)  # %100 accuracy on train data
+
+
+# todo: check for overfiting..
+# prediction:
+# os.mkdir("test")  # put test files in this folder.
+load_img = tf.keras.utils.load_img
+img_to_array = tf.keras.utils.img_to_array
+
+for img_path in os.listdir('test'):
+
+    img = load_img(os.path.join('test', img_path), target_size=(300, 300, 3), keep_aspect_ratio=True)
+    x = img_to_array(img)
+    # x = x / 255  # normalize the data # have done that in model!
+    x = np.expand_dims(x, axis=0)
+    x = np.vstack([x])
+
+    prediction = model.predict(x)
+    if prediction[0] > 0.5:
+        print(prediction, "The file\33[34m", img_path, "\33[38mis a human")
+    else:
+        print(prediction, "The file\33[34m", img_path, "\33[38mis a horse")
